0a48e4a05bce2e17c2da278093737902af5da1ff
David Bishai <dbishai@outlook.com>
2019-04-10 17:00:27 +0000

    add script to download dataset

---
+++ download-dataset.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 2019 David Bishai.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+N=499
+NUM_PROC=6
+
+download_and_compare() {
+  local i=$1
+  images_file_name=images_$1.tar
+  images_md5_file_name=md5.images_$1.txt
+  images_tar_url=https://s3.amazonaws.com/google-landmark/train/$images_file_name
+  images_md5_url=https://s3.amazonaws.com/google-landmark/md5sum/train/$images_md5_file_name
+  echo "Downloading $images_file_name..."
+  curl -Os $images_tar_url > /dev/null
+  curl -Os $images_md5_url > /dev/null
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    images_md5="$(md5sum "$images_file_name")"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    images_md5="$(md5 -r "$images_file_name")"
+  fi
+  md5_1="$(cut -d' ' -f1<<<"$images_md5")"
+  md5_2="$(cut -d' ' -f1<<<cat "$images_md5_file_name")"
+  if [[ "$md5_1" != "" && "$md5_1" = "$md5_2" ]]; then
+    tar -xf ./$images_file_name
+    echo "$images_file_name extracted!"
+  else
+    echo "MD5 checksum for $images_file_name did not match checksum in $images_md5_file_name"
+  fi
+}
+
+for i in $(seq 0 $NUM_PROC $N); do
+  upper=$(expr $i + $NUM_PROC)
+  limit=$(($upper>$N?$N:$upper))
+  for j in $(seq -f "%03g" $i $limit); do download_and_compare "$j" & done
+  wait
+done
